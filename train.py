31ce0a3a31f56cdd1435450ab3022d585b9c5ac2
senpai-kohaku <senpai-kohaku@senpai>
2026-03-18 22:06:37 +0000

    change ema_decay to 0.995 (from 0.998) for more aggressive averaging

--- train.py
+++ train.py
@@ -495,7 +495,7 @@ _base_model = model._orig_mod if hasattr(model, '_orig_mod') else model
 from copy import deepcopy
 ema_model = None
 ema_start_epoch = 40
-ema_decay = 0.998
+ema_decay = 0.995  # was 0.998
 
 n_params = sum(p.numel() for p in model.parameters())
 
